f107cd7914a14705e57bd3bcec3c14a344ce5484
Kelly Ranger <kelly@losgatos-massage.com>
2020-04-30 12:53:11 -0700

    Search Bar and Log components

--- src/components/logs/LogItem.js
+++ src/components/logs/LogItem.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
 
 const LogItem = ({ log }) => {
   return (
     <li className="colletion-item">
       <div>
-        <a href="#">{log.message}</a>
+        <a href="#edit-log-modal"
+          className={`modal-trigger ${
+            log.attention ? "red-text" : "blue-text"}`
+          }>
+          {log.message}
+        </a>
+        <br />
+        <span className="grey-text">
+          <span className="black-text">ID #{log.id}</span>
+          Last updated by: {'  '}
+          <span className="black-text">{log.tech}</span>
+        on <Moment format="MMM Do YYYY, h:mm:ss a">{log.date}</Moment>
+        </span>
+        <a href="#!" className="secondary-content">
+          <i className="material-icon grey-text">delete</i>
+        </a>
       </div>
 
     </li>
-  )
-}
+  );
+};
 
 LogItem.propTypes = {
   log: PropTypes.object.isRequired

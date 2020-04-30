f0461f39407d20a8d1626c5e117af7564d83c90b
Kelly Ranger <kelly@losgatos-massage.com>
2020-04-30 10:35:09 -0700

    first commit to remote

---
+++ src/components/logs/LogItem.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const LogItem = ({ log }) => {
+  return (
+    <li className="colletion-item">
+      <div>
+        <a href="#">{log.message}</a>
+      </div>
+
+    </li>
+  )
+}
+
+LogItem.propTypes = {
+  log: PropTypes.object.isRequired
+}
+
+export default LogItem;
